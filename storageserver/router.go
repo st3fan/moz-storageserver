7467c4fae670ceee81c023280eda57f50fab2f59
Stefan Arentz <stefan@arentz.ca>
2014-09-19 13:49:55 -0400

    First two (empty) storage api handlers

--- storageserver/router.go
+++ storageserver/router.go
@@ -15,36 +15,30 @@ type handlerContext struct {
 	config Config
 }
 
-func (c *handlerContext) GetHawkCredentials(id string) hawk.Credentials {
-	return hawk.Credentials{Key: "keykey", Algorithm: "sha256", User: "stefan"}
+func (c *handlerContext) GetHawkCredentials(r *http.Request, keyIdentifier string) (*hawk.Credentials, error) {
+	return &hawk.Credentials{Key: []byte{1, 2, 3, 4}, Algorithm: "sha256", KeyIdentifier: "abcdef"}, nil
+}
+
+func (c *handlerContext) GetUserIdFromRequest(w http.ResponseWriter, r *http.Request) string {
+	params := mux.Vars(r)
+	return params["userId"]
 }
 
 func (c *handlerContext) InfoCollectionsHandler(w http.ResponseWriter, r *http.Request) {
-	if ok, artifacts, credentials := hawk.Authorize(w, r, c.GetHawkCredentials); ok {
-		log.Printf("Hawk artifacts: %+v", artifacts)
-		log.Printf("Hawk credentials: %+v", credentials)
-		w.Write([]byte(`Cheese`))
+	if _, credentials, ok := hawk.Authorize(w, r, c.GetHawkCredentials); ok {
+		log.Printf("InfoCollectionsHandler! (%+v)", credentials)
 	}
 }
 
-func (c *handlerContext) InfoQuotaHandler(w http.ResponseWriter, r *http.Request) {
-}
-
-func (c *handlerContext) InfoCollectionUsageHandler(w http.ResponseWriter, r *http.Request) {
-}
-
-func (c *handlerContext) InfoCollectionCountsHandler(w http.ResponseWriter, r *http.Request) {
-}
-
 func (c *handlerContext) DeleteAllRecordsHandler(w http.ResponseWriter, r *http.Request) {
+	if _, credentials, ok := hawk.Authorize(w, r, c.GetHawkCredentials); ok {
+		log.Printf("DeleteAllRecordsHandler! (%+v)", credentials)
+	}
 }
 
 func SetupRouter(r *mux.Router, config Config) (*handlerContext, error) {
 	context := &handlerContext{config: config}
-	r.HandleFunc("/{nodeId}/info/collections", context.InfoCollectionsHandler).Methods("GET")
-	r.HandleFunc("/{nodeId}/info/quota", context.InfoQuotaHandler).Methods("GET")
-	r.HandleFunc("/{nodeId}/info/collection_usage", context.InfoCollectionUsageHandler).Methods("GET")
-	r.HandleFunc("/{nodeId}/info/collection_counts", context.InfoCollectionCountsHandler).Methods("GET")
-	r.HandleFunc("/{nodeId}", context.DeleteAllRecordsHandler).Methods("DELETE")
+	r.HandleFunc("/1.5/{userId}/info/collections", context.InfoCollectionsHandler).Methods("GET")
+	r.HandleFunc("/1.5/{userId}", context.DeleteAllRecordsHandler).Methods("DELETE")
 	return context, nil
 }
