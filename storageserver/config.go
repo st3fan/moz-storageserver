e4425a5a392438922e8bbf18f2dbcb9a949ccbb6
Stefan Arentz <stefan@arentz.ca>
2014-09-21 18:12:30 -0400

    Storage server also needs the shared secret?

--- storageserver/config.go
+++ storageserver/config.go
@@ -5,15 +5,18 @@
 package storageserver
 
 const (
-	DEFAULT_DATABASE_URL = "postgres://storageserver:storageserver@localhost/storageserver"
+	DEFAULT_DATABASE_URL  = "postgres://storageserver:storageserver@localhost/storageserver"
+	DEFAULT_SHARED_SECRET = "cheesebaconeggs"
 )
 
 type Config struct {
-	DatabaseUrl string
+	DatabaseUrl  string
+	SharedSecret string
 }
 
 func DefaultConfig() Config {
 	return Config{
-		DatabaseUrl: DEFAULT_DATABASE_URL,
+		DatabaseUrl:  DEFAULT_DATABASE_URL,
+		SharedSecret: DEFAULT_SHARED_SECRET,
 	}
 }
