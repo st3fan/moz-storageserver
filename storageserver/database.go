ac7d502dfedf2be11049480943a0d1fa22cd248b
Stefan Arentz <stefan@arentz.ca>
2014-09-22 15:24:07 -0400

    Implemented more of the Storage Service API

--- storageserver/database.go
+++ storageserver/database.go
@@ -35,7 +35,7 @@ func (session *DatabaseSession) Close() {
 }
 
 func (ds *DatabaseSession) GetCollectionTimestamps(uid uint64) (map[string]float64, error) {
-	rows, err := ds.db.Query("SELECT CollectionName, LastModified FROM UserCollections WHERE UserId=$1", uid)
+	rows, err := ds.db.Query("select Collectionname, max(Modified) from Objects where UserId = $1 group by CollectionName", uid)
 	if err != nil {
 		return nil, err
 	}
@@ -52,4 +52,40 @@ func (ds *DatabaseSession) GetCollectionTimestamps(uid uint64) (map[string]float
 		return nil, err
 	}
 	return result, nil
+}
+
+type Object struct {
+	Id       string  `json:"id"`
+	Modified float64 `json:"modified"`
+	Payload  string  `json:"payload"`
+}
+
+func (ds *DatabaseSession) GetObject(userId uint64, collectionName string, objectId string) (*Object, error) {
+	var modified uint64
+	var object Object
+	err := ds.db.QueryRow("select Id,Modified,Payload from Objects where UserId = $1 and collectionName = $2 and Id = $3", userId, collectionName, objectId).
+		Scan(&object.Id, &modified, &object.Payload)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		} else {
+			return nil, err
+		}
+	}
+	object.Modified = timestampFromInteger(modified)
+	return &object, nil
+}
+
+func (ds *DatabaseSession) GetObjects(userId uint64, limit int, newer float64) ([]Object, error) {
+	return nil, nil
+}
+
+func (ds *DatabaseSession) GetObjectIds(userId uint64, limit int, newer float64) ([]string, error) {
+	return nil, nil
+}
+
+func (ds *DatabaseSession) DeleteAllRecords(userId uint64) error {
+	_, err := ds.db.Exec("delete from Objects where UserId = $1", userId)
+	return err
 }
--- storageserver/router.go
+++ storageserver/router.go
@@ -5,14 +5,40 @@
 package storageserver
 
 import (
-	//"encoding/json"
-	"fmt"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/st3fan/moz-storageserver/hawk"
 	"github.com/st3fan/moz-tokenserver/token"
-	"log"
 	"net/http"
+	"strconv"
 )
+
+// Utility functions to deal with query parameter parsing
+
+func parseLimit(r *http.Request) int {
+	query := r.URL.Query()
+	if len(query["limit"]) != 0 {
+		limit, _ := strconv.Atoi(query["limit"][0])
+		return limit
+	}
+	return 0
+}
+
+func parseFull(r *http.Request) bool {
+	query := r.URL.Query()
+	return len(query["full"]) != 0
+}
+
+func parseNewer(r *http.Request) float64 {
+	query := r.URL.Query()
+	if len(query["newer"]) != 0 {
+		newer, _ := strconv.ParseFloat(query["newer"][0], 64)
+		return newer
+	}
+	return 0
+}
+
+//
 
 type handlerContext struct {
 	config Config
@@ -24,7 +50,6 @@ func (c *handlerContext) GetHawkCredentials(r *http.Request, keyIdentifier strin
 	if err != nil {
 		return nil, err
 	}
-
 	return &hawk.Credentials{
 		Key:           []byte(token.DerivedSecret),
 		Algorithm:     "sha256",
@@ -33,37 +58,93 @@ func (c *handlerContext) GetHawkCredentials(r *http.Request, keyIdentifier strin
 	}, nil
 }
 
-//
+// Handlers
 
 func (c *handlerContext) InfoCollectionsHandler(w http.ResponseWriter, r *http.Request) {
 	if _, credentials, ok := hawk.Authorize(w, r, c.GetHawkCredentials); ok {
-		log.Printf("InfoCollectionsHandler! (%+v)", credentials)
-
 		collectionInfo, err := c.db.GetCollectionTimestamps(credentials.Uid)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		// TODO: Wow why do we still use floats for timestamp? And is there not a better way to do this?
-
-		response := "{"
-		for collectionName, lastModified := range collectionInfo {
-			if len(response) != 1 {
-				response += ", "
-			}
-			response += fmt.Sprintf(`"%s":%.2f`, collectionName, lastModified)
+		encodedObject, err := json.Marshal(collectionInfo)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
-		response += "}"
 
 		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(response))
+		w.Write(encodedObject)
+		return
+	}
+}
+
+func (c *handlerContext) GetObjectHandler(w http.ResponseWriter, r *http.Request) {
+	if _, credentials, ok := hawk.Authorize(w, r, c.GetHawkCredentials); ok {
+		vars := mux.Vars(r)
+		object, err := c.db.GetObject(credentials.Uid, vars["collectionName"], vars["objectId"])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if object == nil {
+			http.Error(w, "Not found", http.StatusNotFound)
+			return
+		}
+
+		encodedObject, err := json.Marshal(object)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(encodedObject)
+	}
+}
+
+func (c *handlerContext) GetObjectsHandler(w http.ResponseWriter, r *http.Request) {
+	if _, credentials, ok := hawk.Authorize(w, r, c.GetHawkCredentials); ok {
+		if parseFull(r) {
+			objects, err := c.db.GetObjects(credentials.Uid, parseLimit(r), parseNewer(r))
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			encodedObject, err := json.Marshal(objects)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(encodedObject)
+		} else {
+			objectIds, err := c.db.GetObjectIds(credentials.Uid, parseLimit(r), parseNewer(r))
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			encodedObject, err := json.Marshal(objectIds)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(encodedObject)
+		}
 	}
 }
 
 func (c *handlerContext) DeleteAllRecordsHandler(w http.ResponseWriter, r *http.Request) {
 	if _, credentials, ok := hawk.Authorize(w, r, c.GetHawkCredentials); ok {
-		log.Printf("DeleteAllRecordsHandler! (%+v)", credentials)
+		err := c.db.DeleteAllRecords(credentials.Uid)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("{}"))
 	}
 }
 
@@ -75,6 +156,8 @@ func SetupRouter(r *mux.Router, config Config) (*handlerContext, error) {
 
 	context := &handlerContext{config: config, db: db}
 	r.HandleFunc("/1.5/{userId}/info/collections", context.InfoCollectionsHandler).Methods("GET")
+	r.HandleFunc("/1.5/{userId}/storage/{collectionName}/{objectId}", context.GetObjectHandler).Methods("GET")
+	r.HandleFunc("/1.5/{userId}/storage/{collectionName}", context.GetObjectsHandler).Methods("GET")
 	r.HandleFunc("/1.5/{userId}", context.DeleteAllRecordsHandler).Methods("DELETE")
 
 	return context, nil
